897d85bb280bce6c5e93c0e682d93245d0641648
Wes Peters <wespeters18@gmail.com>
2024-12-22 14:09:17 -0500

    first commit

---
+++ app.c
@@ -0,0 +1,24 @@
+#include "dm.h"
+
+void dm_application_setup(dm_context_init_packet* init_packet)
+{
+}
+
+bool dm_application_init(dm_context* context)
+{
+    return true;
+}
+
+void dm_application_shutdown(dm_context* context)
+{
+}
+
+bool dm_application_update(dm_context* context)
+{
+    return true;
+}
+
+bool dm_application_render(dm_context* context)
+{
+    return true;
+}
