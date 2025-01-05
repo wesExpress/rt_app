f42cd0182c108e19eaab1f85e2a761e13d00d0cd
Wes Peters <wespeters18@gmail.com>
2025-01-05 13:41:12 -0500

    glsl vertex shader needs negative y values for vulkan.

--- assets/vertex_shader.glsl
+++ assets/vertex_shader.glsl
@@ -4,6 +4,9 @@ layout(location = 0) in vec3 position;
 
 void main()
 {
-    gl_Position = vec4(position, 1.f);
+    gl_Position.x = position.x;
+    gl_Position.y = -position.y;
+    gl_Position.z = position.z;
+    gl_Position.w = 1.f;
 }
 
