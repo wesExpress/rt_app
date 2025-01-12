06ee0fe16b9f8a4e5cc84669adb02ff18c414a46
Wes Peters <wespeters18@gmail.com>
2025-01-12 13:46:33 -0500

    descriptor set working with vulkan??? too easy???

--- app.c
+++ app.c
@@ -6,15 +6,6 @@ typedef struct vertex_t
     float color[4];
 } vertex;
 
-typedef struct application_data_t
-{
-    dm_render_handle raster_pipe;
-    dm_render_handle vb, ib;
-
-    dm_timer frame_timer;
-    uint16_t frame_count;
-} application_data;
-
 const vertex triangle[] = {
     { { -0.5f,-0.5f,0.f }, { 1.f,0.f,0.f,1.f } },
     { {  0.5f,-0.5f,0.f }, { 0.f,1.f,0.f,1.f } },
@@ -30,6 +21,29 @@ const vertex quad[] = {
 
 const uint32_t quad_indices[] = { 0,1,2, 3,2,1 };
 
+typedef struct simple_camera_t
+{
+    dm_vec3 pos, forward, up;
+    dm_mat4 view, proj;
+} simple_camera;
+
+typedef struct camera_uniform_data_t 
+{
+    dm_mat4 mvp;
+} camera_uniform_data;
+
+typedef struct application_data_t
+{
+    dm_render_handle raster_pipe;
+    dm_render_handle vb, ib, cb;
+
+    simple_camera       camera;
+    camera_uniform_data mvp;
+
+    dm_timer frame_timer;
+    uint16_t frame_count;
+} application_data;
+
 void dm_application_setup(dm_context_init_packet* init_packet)
 {
     init_packet->app_data_size = sizeof(application_data);
@@ -38,6 +52,51 @@ void dm_application_setup(dm_context_init_packet* init_packet)
 bool dm_application_init(dm_context* context)
 {
     application_data* app_data = context->app_data;
+
+    // === camera ===
+    {
+        app_data->camera.pos[2] = -3.f;
+        app_data->camera.up[1]  = 1.f;
+        dm_vec3_negate(app_data->camera.pos, app_data->camera.forward);
+        
+        dm_mat_view(app_data->camera.pos, app_data->camera.forward, app_data->camera.up, app_data->camera.view);
+        dm_mat_perspective(65.f, (float)context->renderer.width / (float)context->renderer.height, 0.01f, 1000.f, app_data->camera.proj);
+
+        dm_mat4_mul_mat4(app_data->camera.view, app_data->camera.proj, app_data->mvp.mvp);
+    }
+
+    // === vertex buffer ===
+    {
+        dm_vertex_buffer_desc desc = { 0 };
+        //desc.size         = sizeof(triangle);
+        desc.size         = sizeof(quad);
+        desc.element_size = sizeof(float);
+        desc.stride       = sizeof(vertex);
+        //desc.data         = (void*)triangle;
+        desc.data         = (void*)quad;
+
+        if(!dm_renderer_create_vertex_buffer(desc, &app_data->vb, context)) return false;
+    }
+
+    // === index buffer ===
+    {
+        dm_index_buffer_desc desc = { 0 };
+        desc.size         = sizeof(quad_indices);
+        desc.element_size = sizeof(uint32_t);
+        desc.data         = (void*)quad_indices;
+        desc.index_type   = DM_INDEX_BUFFER_INDEX_TYPE_UINT32;
+
+        if(!dm_renderer_create_index_buffer(desc, &app_data->ib, context)) return false;
+    }
+
+    // === constant buffer ===
+    {
+        dm_constant_buffer_desc desc = { 0 };
+        desc.size = sizeof(camera_uniform_data);
+        desc.data = app_data->mvp.mvp;
+
+        if(!dm_renderer_create_constant_buffer(desc, &app_data->cb, context)) return false;
+    }
 
     // === raster pipeline ===
     {
@@ -84,29 +143,6 @@ bool dm_application_init(dm_context* context)
         if(!dm_renderer_create_raster_pipeline(desc, &app_data->raster_pipe, context)) return false;
     }
 
-    // === vertex buffer ===
-    {
-        dm_vertex_buffer_desc desc = { 0 };
-        //desc.size         = sizeof(triangle);
-        desc.size         = sizeof(quad);
-        desc.element_size = sizeof(float);
-        desc.stride       = sizeof(vertex);
-        //desc.data         = (void*)triangle;
-        desc.data         = (void*)quad;
-
-        if(!dm_renderer_create_vertex_buffer(desc, &app_data->vb, context)) return false;
-    }
-
-    // === index buffer ===
-    {
-        dm_index_buffer_desc desc = { 0 };
-        desc.size         = sizeof(quad_indices);
-        desc.element_size = sizeof(uint32_t);
-        desc.data         = (void*)quad_indices;
-        desc.index_type   = DM_INDEX_BUFFER_INDEX_TYPE_UINT32;
-
-        if(!dm_renderer_create_index_buffer(desc, &app_data->ib, context)) return false;
-    }
 
     // misc
     dm_timer_start(&app_data->frame_timer, context);
@@ -133,12 +169,36 @@ bool dm_application_update(dm_context* context)
         app_data->frame_count++;
     }
 
+    // camera buffer 
+    if(dm_input_is_key_pressed(DM_KEY_W, context))
+    {
+        app_data->camera.pos[2] += 1.f * context->delta;
+    }
+    else if(dm_input_is_key_pressed(DM_KEY_S, context))
+    {
+        app_data->camera.pos[2] -= 1.f * context->delta;
+    }
+
+    if(dm_input_is_key_pressed(DM_KEY_A, context))
+    {
+        app_data->camera.pos[0] -= 1.f * context->delta;
+    }
+    else if(dm_input_is_key_pressed(DM_KEY_D, context))
+    {
+        app_data->camera.pos[0] += 1.f * context->delta;
+    }
+
+    dm_mat_view(app_data->camera.pos, app_data->camera.forward, app_data->camera.up, app_data->camera.view);
+    dm_mat4_mul_mat4(app_data->camera.view, app_data->camera.proj, app_data->mvp.mvp);
+
     return true;
 }
 
 bool dm_application_render(dm_context* context)
 {
     application_data* app_data = context->app_data;
+
+    dm_render_command_update_constant_buffer(app_data->mvp.mvp, sizeof(dm_mat4), app_data->cb, context);
 
     dm_render_command_bind_raster_pipeline(app_data->raster_pipe, context);
     dm_render_command_bind_vertex_buffer(app_data->vb, context);
--- assets/vertex_shader.glsl
+++ assets/vertex_shader.glsl
@@ -1,5 +1,10 @@
 #version 450
 
+layout(binding = 0) uniform UniformBufferObject
+{
+    mat4 mvp;
+} ubo;
+
 layout(location = 0) in vec3 position;
 layout(location = 1) in vec4 in_color;
 
@@ -7,10 +12,9 @@ layout(location = 0) out vec4 out_color;
 
 void main()
 {
-    gl_Position.x = position.x;
-    gl_Position.y = -position.y;
-    gl_Position.z = position.z;
-    gl_Position.w = 1.f;
+    vec4 p = vec4(position.x, position.y, position.z, 1.f);
+    
+    gl_Position = ubo.mvp * p;
 
     out_color = in_color;
 }
